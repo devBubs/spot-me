07ddd238af300e9a41ca034c737e8daf3b6c6c41
rustyswe <naveen.santhanavel@gmail.com>
2023-03-06 09:31:26 +0000

    learnt about string slices and ownership and incorporated in fetch food api

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@
 extern crate rocket;
 
 #[post("/food_log")]
-fn create_entry() -> &'static str {
+fn log_food() -> &'static str {
     "Food logged"
 }
 
 #[get("/food_log/<id>")]
-fn fetch_entry(id: &str) -> &'static str {
-    "Food log fetched- ..."
+fn fetch_food_log(id: &str) -> String {
+    format!("Food log fetched: {}", id)
 }
 
 #[get("/food_log")]
-fn fetch_all_entries() -> &'static str {
+fn fetch_all_food_logs() -> &'static str {
     "Fetched all food logs"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![create_entry, fetch_entry, fetch_all_entries])
+    rocket::build().mount("/", routes![log_food, fetch_food_log, fetch_all_food_logs])
 }
